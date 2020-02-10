5b25fe492f501fcc01e7768738f4a48083ba0c27
DurianPancake <35063674+DurianPancake@users.noreply.github.com>
2020-02-10 11:18:50 +0800

    add gin framework

---
+++ gin/json_test.go
@@ -0,0 +1,54 @@
+package mygin
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"testing"
+)
+
+func TestPostJson(t *testing.T) {
+
+	engine := gin.Default()
+
+	engine.POST(" /student", func(context *gin.Context) {
+		fmt.Println(context.FullPath())
+		var stu Student
+		if err := context.BindJSON(&stu); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("name:", stu.name)
+		fmt.Println("age:", stu.age)
+		_, _ = context.Writer.WriteString("success Register")
+	})
+
+	engine.Run()
+}
+
+type Student struct {
+	name string
+	age  int
+}
+
+func TestReturnJSON(t *testing.T) {
+	engine := gin.Default()
+
+	engine.GET("/helloJson", func(context *gin.Context) {
+		fullPath := "请求路径" + context.FullPath()
+		fmt.Println(fullPath)
+
+		context.JSON(200, map[string]interface{}{
+			"code": 1,
+			"msg":  "ok",
+			"data": fullPath,
+		})
+	})
+
+	engine.GET("/jsonStruct", func(context *gin.Context) {
+		fullPath := "请求路径: " + context.FullPath()
+		fmt.Println(fullPath)
+
+	})
+
+	engine.Run()
+}
