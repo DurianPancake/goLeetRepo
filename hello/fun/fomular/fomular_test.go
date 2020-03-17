b54ac39e5d248a6a9f168f2e8ca73f4e23535ab1
LiuLuhao <luhao.liu@cncommdata.cn>
2020-03-17 19:33:08 +0800

    update formula analysis and compute

--- hello/fun/fomular/fomular_test.go
+++ hello/fun/fomular/fomular_test.go
@@ -1,5 +1,63 @@
 package fomular
 
-var (
-	f1 = "A=3 && B!=4 || C>5"
+import (
+	"fmt"
+	"strings"
+	"testing"
 )
+
+var (
+	f1 = "A=3 && B!=4 ||  C>5"
+	f2 = "A=3 || B!=4 && C>5"
+)
+
+func TestStack(t *testing.T) {
+
+	f1 = strings.ReplaceAll(f1, " ", "")
+	fmt.Println(f1) //A=3&&B!=4||C>5
+
+	stacks := make([]Stack, 0)
+
+	runes := []rune(f1)
+	lastIndex := 0
+	for cursor := 0; cursor < len(runes); cursor++ {
+		offsets := MatchOperator(runes[cursor])
+		// 符号
+		if len(offsets) > 0 {
+			maxOffset, index := 0, -1
+			var op Operator
+			for i, offset := range offsets {
+				// 最大匹配
+				if operator := Match(string(runes[cursor : cursor+offset+1])); operator != "" {
+					if offset >= maxOffset {
+						index = i
+						maxOffset = offset
+						op = operator
+					}
+				}
+			}
+			if index < 0 {
+				continue
+			}
+			// 赋值
+			if lastIndex != cursor {
+				stacks = append(stacks, Stack{
+					Kind:    Field,
+					Field:   string(runes[lastIndex:cursor]),
+					Operate: "",
+					Value:   "",
+				})
+			}
+			stacks = append(stacks, Stack{
+				Kind:    Operate,
+				Field:   "",
+				Operate: op,
+				Value:   "",
+			})
+			lastIndex = cursor
+			cursor += maxOffset
+		}
+	}
+
+	fmt.Println(stacks)
+}
--- hello/fun/fomular/stack.go
+++ hello/fun/fomular/stack.go
@@ -1,31 +1,67 @@
 package fomular
 
 type Stack struct {
-	Kind      _type
-	Condition string
-	Operate   Operator
-	Value     string `json:"value"`
+	Kind    _type
+	Field   string
+	Operate Operator
+	Value   string `json:"value"`
 }
 
 type _type uint8
 
 const (
-	Condition _type = iota
+	Field _type = iota
 	Operate
 	Value
 )
 
 type Operator string
 
+var operators = []Operator{
+	eq,
+	ne,
+	gt,
+	lt,
+	gte,
+	lte,
+	and,
+	or,
+	//not
+	like,
+	in,
+	notIn,
+}
+
 const (
-	eq    = "="
-	gt    = ">"
-	lt    = "<"
-	gte   = ">="
-	lte   = "<="
-	and   = "&&"
-	or    = "||"
-	not   = "!"
-	in    = "in"
-	notIn = "not in"
+	eq    Operator = "="
+	ne    Operator = "!="
+	gt    Operator = ">"
+	lt    Operator = "<"
+	gte   Operator = ">="
+	lte   Operator = "<="
+	and   Operator = "&&"
+	or    Operator = "||"
+	like  Operator = "like"
+	in    Operator = "in"
+	notIn Operator = "not_in"
+	//not
 )
+
+func MatchOperator(char rune) (offset []int) {
+	for _, str := range operators {
+		firstChar := []rune(str)[0]
+		if firstChar == char {
+			offset = append(offset, len(str)-1)
+		}
+	}
+	return
+}
+
+func Match(text string) Operator {
+	for i, operator := range operators {
+		if string(operator) == text {
+			return operators[i]
+		}
+	}
+	return ""
+}
